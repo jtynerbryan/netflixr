a6b34a08b5238b04b9dd992eeafa5a78d9f29ed8
Jesse Tyner-Bryan <jesse@Jesses-Air.frontier.com>
2017-09-03 15:21:48 -0400

    more movie info showing and actors have profile pics now

--- src/actorResults.js
+++ src/actorResults.js
@@ -6,11 +6,14 @@ const ActorResults = (props) => {
 			<div>
 			{props.actors.results.map((actor, index) => {
 				return <div key={index}><h2>{actor.name}</h2>
+				<img width="230px" height="320px" src={"https://image.tmdb.org/t/p/w500/" + actor.profile_path}  alt="Profile pic not available"/>
 							{actor.known_for.map((movie, index) => {
 								return <div key={index}>
-									   	<h4>{movie.title}</h4>
-									   	<h5>Release Date: {movie.release_date}</h5>
-									   	<p>{movie.overview}</p>
+									   	<h1>{movie.title}</h1>
+						<p>Rating: {movie.vote_average}/10 ({movie.vote_count} votes)</p>
+						<h4>Release Date: {movie.release_date}</h4>
+						<img width="240px" height="320px" src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}  alt="Poster not available"/>
+						<p>{movie.overview}</p>
 									   </div>
 							})}
 					   </div>
--- src/movieResults.js
+++ src/movieResults.js
@@ -8,8 +8,9 @@ const MovieResults = (props) => {
 				{props.movies.results.map((movie, index) => { 
 					return <div key={index}>
 						<h1>{movie.title}</h1>
-						<img className="center-block img-responsive" width="240px" height="320px" src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}  alt="Poster not available" data-reactid=".2.1.0.0.1.0.0.0"/>
+						<p>Rating: {movie.vote_average}/10 ({movie.vote_count} votes)</p>
 						<h4>Release Date: {movie.release_date}</h4>
+						<img width="240px" height="320px" src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}  alt="Poster not available"/>
 						<p>{movie.overview}</p>
 					</div>
 				})}
@@ -19,15 +20,3 @@ const MovieResults = (props) => {
 }
 
 export default MovieResults
-
-
-
-// overview
-
-//release_date
-
-				// <h1>{props.movies.results[0].title}</h1>
-				// <h4>Release Date: {props.movies.results[0].release_date}</h4>
-				// <p>{props.movies.results[0].overview}</p>
-
-				//{"images/" + this.props.image}
