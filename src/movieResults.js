4fd48739a068b1191d2471c4c8191167fbd6de01
Jesse Tyner-Bryan <jesse@Jesses-Air.frontier.com>
2017-09-03 12:39:43 -0400

    movie posters in movie search now appearing

--- src/movieResults.js
+++ src/movieResults.js
@@ -4,9 +4,11 @@ const MovieResults = (props) => {
 	console.log(props)
 	return(
 			<div>
+
 				{props.movies.results.map((movie, index) => { 
 					return <div key={index}>
 						<h1>{movie.title}</h1>
+						<img className="center-block img-responsive" width="240px" height="320px" src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}  alt="Poster not available" data-reactid=".2.1.0.0.1.0.0.0"/>
 						<h4>Release Date: {movie.release_date}</h4>
 						<p>{movie.overview}</p>
 					</div>
@@ -27,3 +29,5 @@ export default MovieResults
 				// <h1>{props.movies.results[0].title}</h1>
 				// <h4>Release Date: {props.movies.results[0].release_date}</h4>
 				// <p>{props.movies.results[0].overview}</p>
+
+				//{"images/" + this.props.image}
